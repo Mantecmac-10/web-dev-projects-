80188416588b197d3fc0e610a577fcc7b41ac7ac
Mantecmac-10 <Mantech468@gmail.com>
2026-01-26 13:36:39 +0530

    number guess game

---
+++ Number Guess/script.js
@@ -0,0 +1,29 @@
+let comment = document.getElementById("ques")
+
+let input = document.getElementById("num")
+
+let submitBtn = document.getElementById("sub")
+
+let resetBtn = document.getElementById("res")
+
+
+
+let rand = Math.floor(Math.random()*10)
+
+submitBtn.addEventListener("click" , ()=>{
+    let num = Number(input.value)
+    if(num === rand){
+        comment.textContent="YOU GUESSED THE RIGHT NUMBER !!"
+
+
+    }else{
+        comment.textContent="WRONG GUESS !! TRY AGAIN"
+    }
+})
+
+resetBtn.addEventListener("click", ()=>{
+        comment.textContent= "Enter a Number from 0 to 9."
+        input.value = "" 
+        rand = Math.floor(Math.random()*10)
+})
+

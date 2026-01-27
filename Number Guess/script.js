c29467d57bee3385b5c21dedb560a2184b6cb566
Mantecmac-10 <Mantech468@gmail.com>
2026-01-28 01:41:56 +0530

    chnages in css and js in number guess

--- Number Guess/script.js
+++ Number Guess/script.js
@@ -1,29 +1,28 @@
-let comment = document.getElementById("ques")
+let comment = document.getElementById('ques');
 
-let input = document.getElementById("num")
+let input = document.getElementById('num');
 
-let submitBtn = document.getElementById("sub")
+let submitBtn = document.getElementById('sub');
 
-let resetBtn = document.getElementById("res")
+let resetBtn = document.getElementById('res');
 
+let rand = Math.floor(Math.random() * 10);
 
+submitBtn.addEventListener('click', () => {
+  let num = Number(input.value);
+  if (num === rand) {
+    comment.textContent = 'YOU GUESSED THE RIGHT NUMBER !!';
+  } else if (num > rand) {
+    comment.textContent = 'TOO HIGH!!';
+  } else if (num < rand) {
+    comment.textContent = 'TOO LOW!!';
+  } else {
+    comment.textContent = 'WRONG GUESS !! TRY AGAIN';
+  }
+});
 
-let rand = Math.floor(Math.random()*10)
-
-submitBtn.addEventListener("click" , ()=>{
-    let num = Number(input.value)
-    if(num === rand){
-        comment.textContent="YOU GUESSED THE RIGHT NUMBER !!"
-
-
-    }else{
-        comment.textContent="WRONG GUESS !! TRY AGAIN"
-    }
-})
-
-resetBtn.addEventListener("click", ()=>{
-        comment.textContent= "Enter a Number from 0 to 9."
-        input.value = "" 
-        rand = Math.floor(Math.random()*10)
-})
-
+resetBtn.addEventListener('click', () => {
+  comment.textContent = 'Enter a Number from 0 to 9.';
+  input.value = '';
+  rand = Math.floor(Math.random() * 10);
+});
